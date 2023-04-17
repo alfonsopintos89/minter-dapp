1e931c39d9a51922d27e41ba97a6cae2af3f80f3
Alfonso <alfonso.pintos.89@gmail.com>
2023-04-17 18:10:50 -0300

    goerly public

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x721AA996a9A056Fd015584B763783718B7243e48"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
