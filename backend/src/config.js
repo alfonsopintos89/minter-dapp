da88af7d2627de99e65834f2373fac3f42fe6df9
Alfonso <alfonso.pintos.89@gmail.com>
2023-04-20 10:35:08 -0300

    nueva api

--- backend/src/config.js
+++ backend/src/config.js
@@ -63,7 +63,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "0x5FA9A6e558A05f7eCDDa8DFd64c11feFcEd7dFB6"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x1644ab2231e18f3079e0B33FF892090291358bf8"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
